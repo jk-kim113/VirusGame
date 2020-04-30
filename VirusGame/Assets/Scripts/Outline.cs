4d49d0f7f1c7e747848ffb2b9a2933ebebd9c2b9
jk-kim113 <57799166+jk-kim113@users.noreply.github.com>
2020-04-30 11:24:06 +0900

    Apply Outline Texture

---
+++ VirusGame/Assets/Scripts/Outline.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Outline : MonoBehaviour
+{
+    public Material border;
+    public Material nonBorder;
+
+    void OnMouseOver()
+    {
+        GetComponent<Renderer>().material = border;
+    }
+
+    void OnMouseExit()
+    {
+        GetComponent<Renderer>().material = nonBorder;
+    }
+}
